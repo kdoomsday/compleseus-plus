28f355800ae43348d9dafe69e742ac2f4bee81a6
Eduardo Barrientos <kdoomsday@gmail.com>
2023-12-10 11:33:00 -0400

    Remove some unneeded configs

--- config.el
+++ config.el
@@ -1,4 +1,1 @@
 (add-hook 'marginalia-mode-hook #'all-the-icons-completion-marginalia-setup)
-(add-hook 'embark-collect-mode-hook
-          (lambda()
-            (define-key embark-collect-mode-map (kbd "M-t") 'embark-collect-toggle-marks)))
--- keybindings.el
+++ keybindings.el
@@ -2,4 +2,3 @@
 (spacemacs/set-leader-keys-for-major-mode 'scala-mode "os" 'consult-lsp-symbols)
 (spacemacs/set-leader-keys-for-major-mode 'scala-mode "of" 'consult-lsp-file-symbols)
 (spacemacs/set-leader-keys-for-major-mode 'scala-mode "od" 'consult-lsp-diagnostics)
-(spacemacs/set-leader-keys "hdm" 'describe-mode)
